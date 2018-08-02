3e26de32530b80b3741085e3e45be40983bf70f1
Erick Muñoz <lilrick1014@gmail.com>
2018-08-02 00:52:41 -0500

    dhcpdump

--- Practica 3/dhcpc/DHCPV1.c
+++ Practica 3/dhcpc/DHCPV1.c
@@ -29,9 +29,115 @@ void imprimeTramaDecimal(unsigned char *paq, int len){
 	}
 	printf("\n");
 }
+int dhcp(unsigned char *paq){
 
+	if(paq[240]==53&&paq[241]==1&&paq[242]==3){
+		return 3;
+	}
+	if(paq[240]==53&&paq[241]==1&&paq[242]==2){
+        return 2;
+    }
+    if(paq[240]==53&&paq[241]==1&&paq[242]==1){
+        return 1;
+    }
+    if(paq[240]==53&&paq[241]==1&&paq[242]==5){
+        return 5;
+    }
+    return 0;
+        
+}
+void mac(unsigned char *paq){
+    int i=0;
+    for(i=0;i<7;i++){
+        printf("%.2x", paq[245+i]);
+        
+    }
+}
+void formatoOffer (unsigned char *paq, char *ip){
+
+				char *cacho = strtok(".",ip);
+				while(cacho!=NULL){
+					printf("%s",cacho);
+				}
+				paq[0] = 2;
+		    	paq[8] = 0;
+				paq[9] = 0;
+				//ip_ofertada
+				paq[16] = (int) cacho;
+				paq[17] = (int) cacho;
+				paq[18] = (int) cacho;
+				paq[19] = (int) cacho;
+
+				paq[20] = 192;
+				paq[21] = 168;
+				paq[22] = 1;
+				paq[23] = 100;
+
+				paq[24] = 192;
+				paq[25] = 168;
+				paq[26] = 1;
+				paq[27] = 1;
+
+				paq[236] = 99;
+				paq[237] = 130;
+				paq[238] = 83;
+				paq[239] = 99;
+				//OPTION DHCPOFFER
+				paq[240] = 53;
+				paq[241] = 1;
+				paq[242] = 2;
+				//OPCION SUBNET MASK
+				paq[243] = 1;
+				paq[244] = 4;
+				paq[245] = 0xff;
+				paq[246] = 0xff;
+				paq[247] = 0xff;
+				paq[248] = 0;
+				//OPTION ROUTER
+				paq[249] = 3;
+				paq[250] = 4;
+				paq[251] = 192;
+				paq[252] = 168;
+				paq[253] = 1;
+				paq[254] = 1;
+				//OPTION DNS
+				paq[255] = 6;
+				paq[256] = 4;
+				paq[257] = 148;
+				paq[258] = 204;
+				paq[259] = 103;
+				paq[260] = 2;
+				//OPT BROADCAST
+				paq[261] = 28;
+				paq[262] = 4;
+				paq[263] = 192;
+				paq[264] = 168;
+				paq[265] = 1;
+				paq[266] = 255;
+				//OPTION ID SERVIDOR
+				paq[267] = 0x36;
+				paq[268] = 4;
+				paq[269] = 192;
+				paq[270] = 168;
+				paq[271] = 1;
+				paq[272] = 100;
+
+				paq[273] = 0xff;
+}
+int esRequest(unsigned char *paq){
+	if(paq[240]==53&&paq[241]==1&&paq[242]==3){
+		return 1;
+	}
+	else{
+		return 0;
+	}
+}
+void guardarIP(unsigned char *paq){
+    
+}
 int main(int argc, char const *argv[]){
-    unsigned char dhcp_discover[343], dhcp_offer[274], dhcp_ack[280], opc_ack[1] = {0x03};
+    unsigned char dhcp_discover[361], dhcp_offer[274], dhcp_ack[280], opc_ack[1] = {0x03}, lease[10][8];
+    //recuerda agregar la mac al lease y despues validar en el request si es que ya le habias asignado algo a ese dispositivo
     int ds, ds2, tamrecv, tamsend, on = 1;
     socklen_t clilen, serverlen;
     struct sockaddr_in server_addr, client_addr;
@@ -56,96 +162,35 @@ int main(int argc, char const *argv[]){
 		clilen = sizeof(client_addr);
 	    while(1){
 	    	tamrecv = recvfrom(ds, dhcp_discover, sizeof(dhcp_discover), 0, (struct sockaddr *)&client_addr, &clilen);
-			imprimeTramaDecimal(dhcp_offer,235);
 		    if(tamrecv < 0){
 		    		perror("No envia DHCPDISCOVER");
 			}else{
 				printf("Exito al recibir DHCPDISCOVER\n");
 				int i;
-				printf("Imprimiendo discover \n\n  ");
-				imprimeTramaDecimal(dhcp_discover,160);
+				if(esRequest(dhcp_discover)){
+					printf("es request");
+				}
+				printf("Imprimiendo Discover \n\n  ");
+                imprimeTrama(dhcp_discover,360);
 				memcpy(dhcp_offer, dhcp_discover, 160);
 
-				//Offer
-				dhcp_offer[0] = 2;
-		    	dhcp_offer[8] = 0;
-				dhcp_offer[9] = 0;
-
-				dhcp_offer[16] = 192;
-				dhcp_offer[17] = 168;
-				dhcp_offer[18] = 1;
-				dhcp_offer[19] = 10;
-
-				dhcp_offer[20] = 192;
-				dhcp_offer[21] = 168;
-				dhcp_offer[22] = 1;
-				dhcp_offer[23] = 100;
-
-				dhcp_offer[24] = 192;
-				dhcp_offer[25] = 168;
-				dhcp_offer[26] = 1;
-				dhcp_offer[27] = 1;
-
-				dhcp_offer[236] = 99;
-				dhcp_offer[237] = 130;
-				dhcp_offer[238] = 83;
-				dhcp_offer[239] = 99;
-				//OPTION DHCPOFFER
-				dhcp_offer[240] = 53;
-				dhcp_offer[241] = 1;
-				dhcp_offer[242] = 2;
-				//OPCION SUBNET MASK
-				dhcp_offer[243] = 1;
-				dhcp_offer[244] = 4;
-				dhcp_offer[245] = 0xff;
-				dhcp_offer[246] = 0xff;
-				dhcp_offer[247] = 0xff;
-				dhcp_offer[248] = 0;
-				//OPTION ROUTER
-				dhcp_offer[249] = 3;
-				dhcp_offer[250] = 4;
-				dhcp_offer[251] = 192;
-				dhcp_offer[252] = 168;
-				dhcp_offer[253] = 1;
-				dhcp_offer[254] = 1;
-				//OPTION DNS
-				dhcp_offer[255] = 6;
-				dhcp_offer[256] = 4;
-				dhcp_offer[257] = 148;
-				dhcp_offer[258] = 204;
-				dhcp_offer[259] = 103;
-				dhcp_offer[260] = 2;
-				//OPT BROADCAST
-				dhcp_offer[261] = 28;
-				dhcp_offer[262] = 4;
-				dhcp_offer[263] = 192;
-				dhcp_offer[264] = 168;
-				dhcp_offer[265] = 1;
-				dhcp_offer[266] = 255;
-				//OPTION ID SERVIDOR
-				dhcp_offer[267] = 0x36;
-				dhcp_offer[268] = 4;
-				dhcp_offer[269] = 192;
-				dhcp_offer[270] = 168;
-				dhcp_offer[271] = 1;
-				dhcp_offer[272] = 100;
-
-				dhcp_offer[273] = 0xff;
+				
 
 				tamsend = sendto(ds, dhcp_offer, 274, 0, (struct sockaddr *)&server_addr, clilen);
 				if(tamsend < 0){
 		    		perror("\nError en sendto");
 		    	}else{
 		    		printf("Exito al enviar DHCPOFFER\n");
-					printf("imprime discover \n ");
-					imprimeTramaDecimal(dhcp_offer,235);
+					printf("imprime DHCPOFFER \n ");
+				
 		    		//Request
     				tamrecv = recvfrom(ds, dhcp_discover, sizeof(dhcp_discover), 0, (struct sockaddr *)&client_addr, &clilen);
     				if(tamrecv < 0){
     					perror("\nError al recibir DHCPREQUEST");
     				}else{
     					printf("Exito al recibir DHCPREQUEST\n");
-						
+						mac(dhcp_discover);
+
     					memcpy(dhcp_ack, dhcp_discover, 235);
     					//ACK
 				    	dhcp_ack[0] = 2;
@@ -220,13 +265,11 @@ int main(int argc, char const *argv[]){
 
 						dhcp_ack[279] = 0xff;
 						if(!memcmp(dhcp_discover+242, opc_ack, 1)){
-							imprimeTramaDecimal(dhcp_ack,280);
 							tamsend = sendto(ds, dhcp_ack, 280, 0, (struct sockaddr *)&server_addr, clilen);
 							if(tamsend < 0){
 								perror("\nError al enviar DHCPACK");
 							}else{
 								printf("Exito al enviar DHCPACK\n");
-								imprimeTramaDecimal(dhcp_ack,280);
 							}
 						}
     				}
